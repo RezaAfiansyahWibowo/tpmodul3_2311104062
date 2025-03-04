1dcd811e033df449a1871ca74e73733e1253c531
Reza <reza09082016@gmail.com>
2025-03-04 13:33:41 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Modul_3_KPL_REZA
+{
+    class DoorMachine
+    {
+        private enum State { Terkunci, Terbuka }
+        private State currentState;
+
+        public DoorMachine()
+        {
+            // State awal adalah Terkunci
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.Terkunci)
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.Terbuka)
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
+//using TP_Modul_3_KPL_REZA;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Memanggil method getKodePos dari class KodePos
+//        KodePos kodePos = new KodePos();
+//        Console.WriteLine("Kode Pos Batununggal: " + kodePos.getKodePos("Batumunggal"));
+//        Console.WriteLine("Kode Pos Kujangsari: " + kodePos.getKodePos("Kujangsari"));
+//        Console.WriteLine("Kode Pos Mengger: " + kodePos.getKodePos("Mengger"));
+//    }
+//}
+
+using System;
 using TP_Modul_3_KPL_REZA;
 
-class Program
+namespace tpmodul3_NIM
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Memanggil method getKodePos dari class KodePos
-        KodePos kodePos = new KodePos();
-        Console.WriteLine("Kode Pos Batununggal: " + kodePos.getKodePos("Batumunggal"));
-        Console.WriteLine("Kode Pos Kujangsari: " + kodePos.getKodePos("Kujangsari"));
-        Console.WriteLine("Kode Pos Mengger: " + kodePos.getKodePos("Mengger"));
+        static void Main(string[] args)
+        {
+            // Memanggil method getKodePos dari class KodePos
+            KodePos kodePos = new KodePos();
+            Console.WriteLine("Kode Pos Batununggal: " + kodePos.getKodePos("Batumunggal"));
+            Console.WriteLine("Kode Pos Kujangsari: " + kodePos.getKodePos("Kujangsari"));
+            Console.WriteLine("Kode Pos Mengger: " + kodePos.getKodePos("Mengger"));
+
+            // Mensimulasikan perubahan state pada DoorMachine
+            DoorMachine door = new DoorMachine();
+            door.KunciPintu(); // State awal: Terkunci
+            door.BukaPintu();  // State berubah: Terbuka
+            door.KunciPintu(); // State berubah: Terkunci
+        }
     }
 }
+    
